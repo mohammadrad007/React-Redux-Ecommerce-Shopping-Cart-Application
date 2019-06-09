adf92e75f673c5fcf509deed65727511785c822e
Mohammad Mohammadi <saberzx313@gmail.com>
2019-06-09 20:05:03 +0430

    fetch from local host by json-server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React, { Component } from "react";
 import Products from "./components/Products";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+      filteredProducts: []
+    };
+  }
+  componentWillMount() {
+    fetch(`http://localhost:8000/products`)
+      .then(res => res.json())
+      .then(data =>
+        this.setState({
+          products: data,
+          filteredProducts: data
+        })
+      );
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div className="container">
         <h1>Ecommerce Shoppin Cart Application</h1>

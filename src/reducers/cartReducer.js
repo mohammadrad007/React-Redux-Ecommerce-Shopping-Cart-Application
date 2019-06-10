0ad481d53ce1825e88efddfbb3744a625977ee80
Mohammad Mohammadi <saberzx313@gmail.com>
2019-06-10 20:31:28 +0430

    cartActions addToCart

---
+++ src/reducers/cartReducer.js
@@ -0,0 +1,17 @@
+import { REMOVE_FROM_CART, ADD_TO_CART } from "../actions/types";
+
+const initialState = {
+  items: []
+};
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case ADD_TO_CART:
+      return {
+        ...state,
+        items: action.payload
+      };
+
+    default:
+      return state;
+  }
+}

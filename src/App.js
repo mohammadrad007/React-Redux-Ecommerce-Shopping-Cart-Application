a0720bd1fc43f0945d632dab6d5da45a561f784d
Mohammad Mohammadi <saberzx313@gmail.com>
2019-06-10 20:17:58 +0430

    clear app.js and state constructor and ...

--- src/App.js
+++ src/App.js
@@ -5,29 +5,6 @@ import Basket from "./components/Basket";
 import { Provider } from "react-redux";
 import store from "./Store";
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // products: [],
-      // filteredProducts: [],
-      cartItems: []
-    };
-  }
-  // componentWillMount() {
-  //   fetch(`http://localhost:8000/products`)
-  //     .then(res => res.json())
-  //     .then(data =>
-  //       this.setState({
-  //         products: data,
-  //         filteredProducts: data
-  //       })
-  //     );
-  //   if (localStorage.getItem("cartItems")) {
-  //     this.setState({
-  //       cartItems: JSON.parse(localStorage.getItem("cartItems"))
-  //     });
-  //   }
-  // }
   handleAddToCart = (e, product) => {
     this.setState(state => {
       const cartItems = state.cartItems;
@@ -53,48 +30,6 @@ class App extends Component {
     });
   };
 
-  handleChangeSort = e => {
-    this.setState({
-      sort: e.target.value
-    });
-    this.listProducts();
-  };
-
-  handleChangeSize = e => {
-    this.setState({ size: e.target.value });
-    this.listProducts();
-  };
-
-  listProducts = () => {
-    this.setState(state => {
-      if (state.sort !== "") {
-        state.products.sort((a, b) =>
-          state.sort === "lowest"
-            ? a.price > b.price
-              ? 1
-              : -1
-            : a.price < b.price
-            ? 1
-            : -1
-        );
-      } else {
-        state.products.sort((a, b) => (a.id < b.id ? 1 : -1));
-      }
-
-      if (state.size !== "") {
-        return {
-          filteredProducts: state.products.filter(
-            product =>
-              product.availableSizes.indexOf(state.size.toUpperCase()) > -1
-          )
-        };
-      }
-      return {
-        filteredProducts: state.products
-      };
-    });
-  };
-
   render() {
     // console.log(this.state);
     return (
@@ -104,19 +39,13 @@ class App extends Component {
           <hr />
           <div className="row">
             <div className="col-md-8">
-              <Filter
-              // size={this.state.size}
-              // sort={this.state.sort}
-              // handleChangeSize={this.handleChangeSize}
-              // handleChangeSort={this.handleChangeSort}
-              // count={this.state.filteredProducts.length}
-              />
+              <Filter />
               <hr />
               <Products handleAddToCart={this.handleAddToCart} />
             </div>
             <div className="col-md-4">
               <Basket
-                cartItems={this.state.cartItems}
+                // cartItems={this.state.cartItems}
                 handleRemoveFromCart={this.handleRemoveFromCart}
               />
             </div>

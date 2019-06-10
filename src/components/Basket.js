605ff294a5d075d0d8080154313f8a836e5a7c73
Mohammad Mohammadi <saberzx313@gmail.com>
2019-06-10 16:11:53 +0430

    Total price use util and format currency

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import util from "../util";
 
 export default class Basket extends Component {
   render() {
@@ -15,7 +16,7 @@ export default class Basket extends Component {
             <ul>
               {cartItems.map(item => (
                 <li key={item.id}>
-                  <b>{item.title}</b>X {item.count}
+                  <b>{item.title}</b>X {item.count} = {item.price * item.count}
                   <button
                     className="btn btn-danger"
                     onClick={e => handleRemoveFromCart(e, item)}
@@ -25,6 +26,10 @@ export default class Basket extends Component {
                 </li>
               ))}
             </ul>
+            Total :{" "}
+            {util.formatCurency(
+              cartItems.reduce((a, c) => a + c.price * c.count, 0)
+            )}
           </div>
         )}
       </div>
